8f97563cd9b6788c72b9b159d54b47565ce27af7
Daniel <mock@Mock.local>
2018-10-20 19:31:24 -0700

    packages

---
+++ client/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Reader from '../Reader/Reader';
+import './App.scss';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Credit Proxy</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
